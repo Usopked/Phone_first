68317c8c8c0c7af32862d00e73315402b2bd656b
usopked <usopked16496@gmail.com>
2024-08-09 17:40:34 +0900

    second page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.js'; 
+import About from './pages/About.js';
 
 
 function App() {
@@ -26,7 +27,11 @@ function App() {
         </header>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* 다른 라우트도 여기에 추가할 수 있습니다 */}
+          <Route path="/about" element={<About />} />
+          {
+          /* 다른 라우트도 여기에 추가할 수 있습니다 */
+            
+          }
         </Routes>
       </div>
     </Router>
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Welcome to the About Page</h1>
+      <p>This is the sub landing page of the application.</p>
+    </div>
+  );
+};
+
+export default About;
